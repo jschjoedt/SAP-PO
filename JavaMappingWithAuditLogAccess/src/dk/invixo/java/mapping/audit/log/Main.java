3c6cf0bf7909deb401d27d4534b569fdd763efcf
Jesper Schjødt <jesper.schjoedt@invixo.com>
2021-09-24 11:50:07 +0200

    Refactoring

--- JavaMappingWithAuditLogAccess/src/dk/invixo/java/mapping/audit/log/Main.java
+++ JavaMappingWithAuditLogAccess/src/dk/invixo/java/mapping/audit/log/Main.java
@@ -1,7 +1,5 @@
 package dk.invixo.java.mapping.audit.log;
 
-import java.util.UUID;
-
 import com.sap.aii.mapping.api.AbstractTransformation;
 import com.sap.aii.mapping.api.StreamTransformationException;
 import com.sap.aii.mapping.api.TransformationInput;
@@ -14,25 +12,15 @@ import com.sap.engine.interfaces.messaging.api.auditlog.AuditLogStatus;
 import com.sap.engine.interfaces.messaging.api.exception.MessagingException;
 
 public class Main extends AbstractTransformation {
+	
+	private static final String DASH = "-";
 
 	@Override
 	public void transform(TransformationInput in, TransformationOutput out) throws StreamTransformationException {
 		// Retrieve message ID from input header (com.sap.aii.mapping.api.InputHeader)
 		// of transformation input (com.sap.aii.mapping.api.TransformationInput).
 		String msgID = in.getInputHeader().getMessageId();
-		// Convert message ID to UUID format (in compliance to RFC 4122).
-		// UUID format is used by Advanced Adapter Engine for identifiers of processed messages.
-		// For the sake of simplicity, conversion is done manually - alternatively, specific libraries can be used for this.
-		final String DASH = "-";
-		String uuidTimeLow = msgID.substring(0, 8);
-		String uuidTimeMid = msgID.substring(8, 12);
-		String uuidTimeHighAndVersion = msgID.substring(12, 16);
-		String uuidClockSeqAndReserved = msgID.substring(16, 18);
-		String uuidClockSeqLow = msgID.substring(18, 20);
-		String uuidNode = msgID.substring(20, 32);
-		String msgUUID =
-		     uuidTimeLow + DASH + uuidTimeMid + DASH + uuidTimeHighAndVersion + DASH
-		     + uuidClockSeqAndReserved + uuidClockSeqLow + DASH + uuidNode;
+		String msgUUID = generateMessageUUID(msgID);
 		// Construct message key (com.sap.engine.interfaces.messaging.api.MessageKey)
 		// for retrieved message ID and outbound message direction (com.sap.engine.interfaces.messaging.api.MessageDirection).
 		MessageKey msgKey = new MessageKey(msgUUID, MessageDirection.OUTBOUND);
@@ -50,6 +38,25 @@ public class Main extends AbstractTransformation {
 		          e.printStackTrace();
 		     }
 		
+	}
+
+	private String generateMessageUUID(String msgID) {
+		// Convert message ID to UUID format (in compliance to RFC 4122).
+		// UUID format is used by Advanced Adapter Engine for identifiers of processed messages.
+		// For the sake of simplicity, conversion is done manually - alternatively, specific libraries can be used for this.
+		
+		String uuidTimeLow = msgID.substring(0, 8);
+		String uuidTimeMid = msgID.substring(8, 12);
+		String uuidTimeHighAndVersion = msgID.substring(12, 16);
+		String uuidClockSeqAndReserved = msgID.substring(16, 18);
+		String uuidClockSeqLow = msgID.substring(18, 20);
+		String uuidNode = msgID.substring(20, 32);
+		
+		String msgUUID =
+		     uuidTimeLow + DASH + uuidTimeMid + DASH + uuidTimeHighAndVersion + DASH
+		     + uuidClockSeqAndReserved + uuidClockSeqLow + DASH + uuidNode;
+		
+		return msgUUID;
 	}
 
 }
